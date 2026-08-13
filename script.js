51f691bfb5b7579f47792170aa2cf1866cf95eb2
marucafe <guanglaisishi@gmail.com>
2026-08-13 18:59:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 const cafes = [
   {
     id: 1,
-    name: "Cafe Sample",
-    image: "https://placehold.co/600x400?text=Cafe+Photo",
-    tags: ["Wi-Fi", "PayPay", "コーヒー"]
+    name: "Cafe&Bar TRINITY",
+    image: "trinity(1).jpg",
+    walk: "丸亀駅から徒歩2分",
+    tags: ["PayPay", "Wi-Fi"]
   }
 ];
 
 const cafeList = document.getElementById("cafeList");
+const searchInput = document.getElementById("searchInput");
+const tagButtons = document.querySelectorAll(".tag");
 
+
+// カフェ一覧を表示
 function displayCafes(list) {
 
     cafeList.innerHTML = "";
+
+    if (list.length === 0) {
+        cafeList.innerHTML = `
+            <p class="no-result">
+                該当する店舗がありません。
+            </p>
+        `;
+        return;
+    }
 
     list.forEach(cafe => {
 
@@ -23,6 +37,10 @@ function displayCafes(list) {
             <div class="card-body">
 
                 <h3>${cafe.name}</h3>
+
+                <p class="walk">
+                    🚶 ${cafe.walk}
+                </p>
 
                 <div class="card-tags">
 
@@ -38,7 +56,40 @@ function displayCafes(list) {
         `;
 
     });
-
 }
 
+
+// 店名検索
+searchInput.addEventListener("input", function() {
+
+    const keyword = searchInput.value.toLowerCase();
+
+    const filteredCafes = cafes.filter(cafe =>
+        cafe.name.toLowerCase().includes(keyword)
+    );
+
+    displayCafes(filteredCafes);
+
+});
+
+
+// タグ検索
+tagButtons.forEach(button => {
+
+    button.addEventListener("click", function() {
+
+        const selectedTag = this.textContent;
+
+        const filteredCafes = cafes.filter(cafe =>
+            cafe.tags.includes(selectedTag)
+        );
+
+        displayCafes(filteredCafes);
+
+    });
+
+});
+
+
+// 最初に店舗を表示
 displayCafes(cafes);
